7c7202b98406c09b52f7717517b0d5668d6f46b3
ClaraLeh <clara.lehenaff@innerseas.com>
2021-05-03 17:49:19 +0200

    pymed comments and example
    Commenting the pymed_for_oplr script, and commiting a pickle example

--- pymed_for_oplr.py
+++ pymed_for_oplr.py
@@ -2,11 +2,14 @@ from tqdm import tqdm
 from pymed import PubMed
 import pickle
 
+# change the query with your own ! [CHANGE QUERY]
 pubmed = PubMed(tool="OPLR", email="clara.lehenaff@cri-paris.org")
 results = pubmed.query('"trans women" OR "trans woman" OR "trans man" OR "trans men" OR "transwoman" OR "transwomen" OR "transmen" OR "transman" OR "transgender" OR "transsexual" OR "transgenderism" OR "transsexuality" OR "transsexualism"', max_results=5)
 
 mydict = {"pubs":{},"labels":{}}
 
+# Here we construct an OPLR dictionary file based on the pubmed API :
+# here are the fields, only fields present in both articles and books ar used. (copyrights is not used)
 # BOTH : "pubmed_id" "title" "abstract" "publication_date" "authors" "copyrights" "doi"
 # ARTI : "keywords" "journal" "methods" "conclusions" "results" "xml"
 # BOOK : "doi" "isbn" "language" "publication_type" "sections" "publisher" "publisher_location"
