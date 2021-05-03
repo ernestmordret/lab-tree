2c346657c4bbb4d3cab5aabbec4003796c6c65f9
ClaraLeh <clara.lehenaff@innerseas.com>
2021-05-03 17:44:23 +0200

    pymed_for_oplr
    added a new script to transform a PUBMED query into an OPLR file

---
+++ pymed_for_oplr.py
@@ -0,0 +1,29 @@
+from tqdm import tqdm
+from pymed import PubMed
+import pickle
+
+pubmed = PubMed(tool="OPLR", email="clara.lehenaff@cri-paris.org")
+results = pubmed.query('"trans women" OR "trans woman" OR "trans man" OR "trans men" OR "transwoman" OR "transwomen" OR "transmen" OR "transman" OR "transgender" OR "transsexual" OR "transgenderism" OR "transsexuality" OR "transsexualism"', max_results=5)
+
+mydict = {"pubs":{},"labels":{}}
+
+# BOTH : "pubmed_id" "title" "abstract" "publication_date" "authors" "copyrights" "doi"
+# ARTI : "keywords" "journal" "methods" "conclusions" "results" "xml"
+# BOOK : "doi" "isbn" "language" "publication_type" "sections" "publisher" "publisher_location"
+for pub in tqdm(results):
+    authors = []
+    for a in pub.authors:
+        name = a['initials']+" "+a['lastname']
+        authors.append(name)
+    
+    date = pub.publication_date.strftime("%Y-%m-%d")
+    
+    bib = {'abstract':pub.abstract,
+           'author':authors,
+           'pub_year':date,
+           'title':pub.title}
+    
+    mydict["pubs"][int(pub.pubmed_id)] = {'bib':bib, 'pub_url':pub.doi}
+
+with open('transhealth.pickle', 'wb') as f:
+    pickle.dump(mydict, f, pickle.HIGHEST_PROTOCOL)
