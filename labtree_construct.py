4854a8e9cc47f9b2476ddcb1cc141428ce6cf015
ClaraLeh <clara.lehenaff@innerseas.com>
2021-05-05 16:42:49 +0200

    labtree_construct simplified
    all the values the user need to change are now in a simple box at the beginning.

--- labtree_construct.py
+++ labtree_construct.py
@@ -2,8 +2,18 @@ import pickle
 from scholarly import scholarly
 from tqdm import tqdm
 
+###############################################
+# THESE ARE ALL THE VALUES YOU NEED TO CHANGE #
+###############################################
+file_to_open = "oxman2.pickle" # this is the file you want to modify
+file_to_save = "oxman2.pickle" # the name you want to save. Can be the same as file_to_open
+myauthor = "N Oxman" # the exact name of your author (google scholar format : Initial-space-lastname)
+removepapers = True # True/False if you want to remove the papers where your author is not first or last
+displayall = False # True/False if you want to print all the authors list and the publications with no abstract
+###############################################
+
 # we open the file created by labtree_generate [CHANGE FILE NAME]
-with open('oxman2.pickle', 'rb') as f:
+with open(file_to_open, 'rb') as f:
     # The protocol version used is detected automatically, so we do not
     # have to specify it.
     mydict = pickle.load(f)
@@ -17,30 +27,32 @@ else:
  
 selected={}
 n=0
+toprint = ""
 # we only keep the publications where the author is the one we want [CHANGE AUTHOR NAME]
 for pub in masterdict["pubs"]:
-    if 'N Oxman' in masterdict["pubs"][pub]['bib']['author']:
+    if myauthor in masterdict["pubs"][pub]['bib']['author']:
         n+=1
         selected[n] = masterdict["pubs"][pub]
+toprint = toprint + "Author was present in " + str(n) + " papers out of " + str(len(masterdict["pubs"])) + ", removed the others."
 
 # remove papers where your author is not the first or the last author
 # change removepapers to True if you want to use this function
 # change myauthor by the name of your author
-removepapers = False
-myauthor = "N Oxman"
+m=n
 if removepapers:
     for pub in list(selected):
-        print(selected[pub]['bib']['author'])
         if selected[pub]['bib']['author'][0] != myauthor and selected[pub]['bib']['author'][-1] != myauthor:
             selected.pop(pub,None)
-            print("removed an entry")
+            m-=1
+    toprint = toprint+"\n"+"Author was first or last in "+str(n)+" papers out of "+str(m)+", removed the others."
             
 # we display the list of authors to check if it's right
-for pub in selected:
-    print(selected[pub]['bib']['author'])
-    if "abstract" not in selected[pub]['bib']:
-        print(selected[pub]['bib'])
-print(n)
+if displayall:
+    for pub in selected:
+        print(selected[pub]['bib']['author'])
+        if "abstract" not in selected[pub]['bib']:
+            print(selected[pub]['bib'])
+
 
 ################### THIS SECTION RARELY WORK
 ################### because google scholar doesn't want
@@ -65,5 +77,7 @@ print(n)
 # then when we're done, we update masterdict with the new pubs
 masterdict["pubs"] = selected
 
-with open('oxman2.pickle', 'wb') as f:
+print(toprint)
+
+with open(file_to_save, 'wb') as f:
     pickle.dump(masterdict, f, pickle.HIGHEST_PROTOCOL)
